0062ed1ba3593579101dfbf72c1848b599d7aabf
unknown <BharatM@bharatm.talentica-all.com>
2016-02-26 17:39:22 +0530

    Completed first phase of parsing library

--- example/parse.js
+++ example/parse.js
@@ -1,5 +1,5 @@
 'use strict';
-const bigFileParser = require('./lib/bigFileParser');
+const bigFileParser = require('../lib/bigFileParser');
 const dataProcessor = require('./processData');
 const filePathWithName = process.argv[2];
 
@@ -7,5 +7,4 @@ const filePathWithName = process.argv[2];
 let myParser = new bigFileParser(filePathWithName);
 
 myParser.parse();
-
- 
+myParser.on('line', (line) => console.log("Line received :"+ line));
